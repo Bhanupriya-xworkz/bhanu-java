f92c934554e30152cc8df7aff70e758d96f4f6d9
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-03-19 10:05:57 +0530

    adding Bhanu file

--- Java/src/com/xworkz/intern/bhanu.java
+++ Java/src/com/xworkz/intern/bhanu.java
@@ -1,7 +1,7 @@
 package com.xworkz.intern;
 
 public class bhanu {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("hello there");
     }
 }
