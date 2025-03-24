f5eb23d5ee0cbe457519661faf251ee7b663f539
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-03-24 21:34:53 +0530

    add Country file

--- Assess/src/com/xworkz/initusingconstructor/Bar.java
+++ Assess/src/com/xworkz/initusingconstructor/Bar.java
@@ -1,14 +1,12 @@
 package com.xworkz.initusingconstructor;
-public class bar {
+public class Bar {
     String name;
     String owner;
     int investment;
 
-    bar (String name, String owner, int investment){
+    Bar (String name, String owner, int investment){
         this.name = name;
         this.owner = owner;
         this.investment = investment;
     }
-
-
 }
