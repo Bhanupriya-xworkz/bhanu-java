f014245c32eb8d533d9f9c0cee0c1535a2dc10d5
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-03-24 21:16:25 +0530

    add Country file

---
+++ Assess/src/com/xworkz/initusingconstructor/Anklet.java
@@ -0,0 +1,12 @@
+package com.xworkz.initusingconstructor;
+public class Anklet {
+    String material ;
+    int length;
+    double price;
+
+    Anklet(String material, int length, double price){
+        this.material = material;
+        this.length = length;
+        this.price = price;
+    }
+}
