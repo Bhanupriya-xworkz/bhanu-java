bcd163757c70b050ef4f7cd6e52cf62c35d2450a
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-03-24 22:19:39 +0530

    add Country file

---
+++ Assess/src/com/xworkz/MethodsHospital/HospitalRunner.java
@@ -0,0 +1,14 @@
+package com.xworkz.MethodsHospital;
+
+public class HospitalRunner {
+    public static void main(String[] args){
+        String patientName = "Roopa";
+        int age= 32;
+        long pNo = 884585;
+        String email = "roopa@gmail.com";
+        boolean sickness = false;
+
+        Hospital.checkUp(patientName,age,pNo, email, sickness);
+    }
+}
+
