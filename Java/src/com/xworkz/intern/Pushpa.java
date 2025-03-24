e6b3e867d78eb8723268c5e52f4053ee774fa26b
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-03-24 20:40:34 +0530

    add Country file

---
+++ Java/src/com/xworkz/intern/Pushpa.java
@@ -0,0 +1,21 @@
+package com.xworkz.intern;
+
+public class Pushpa {
+    Clip[] clips;
+
+    public Pushpa(Clip[] clips){
+        this.clips = clips;
+    }
+    void useClip(Clip[] clips){
+        for(Clip clip : clips){
+            if(clip != null){
+                System.out.println("Clip color : "+clip.color);
+                clip.hold();
+            }
+            else{
+                System.err.println("The value of clip is null");
+            }
+        }
+    }
+}
+
