f1f4f4c58d8d95de153f99073f1505cce9271268
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-03-24 22:15:36 +0530

    add Country file

---
+++ Assess/src/com/xworkz/MethodsHospital/Doctor.java
@@ -0,0 +1,10 @@
+package com.xworkz.MethodsHospital;
+
+public class Doctor {
+    public static void treatment(String patientName, int age){
+        System.out.println("Running treatment in Doctor");
+        System.out.println("Patient name : "+patientName);
+        System.out.println("Age : "+age);
+        Nurse.assist(patientName);
+    }
+}
