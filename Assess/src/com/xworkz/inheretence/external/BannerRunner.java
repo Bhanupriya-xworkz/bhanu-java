ad22d1005ad95697cab687cf5fa47a2e39df2243
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-04-03 10:11:20 +0530

    add PineApple file

---
+++ Assess/src/com/xworkz/inheretence/external/BannerRunner.java
@@ -0,0 +1,12 @@
+package com.xworkz.inheretence.external;
+import com.xworkz.inheretence.internal.Banner;
+import com.xworkz.inheretence.internal.DigitalBanner;
+
+public class BannerRunner {
+        public static void main(String[] args) {
+
+            Banner banner = new DigitalBanner();
+
+            Banner basicBanner = new Banner();
+        }
+    }
