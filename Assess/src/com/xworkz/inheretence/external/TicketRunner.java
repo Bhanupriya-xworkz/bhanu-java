6acf914fe514b2ffaddd6aae0954e8545eaa9a82
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-04-03 18:11:15 +0530

    add Ticket file

---
+++ Assess/src/com/xworkz/inheretence/external/TicketRunner.java
@@ -0,0 +1,13 @@
+package com.xworkz.inheretence.external;
+
+import com.xworkz.inheretence.internal.DigitalTicket;
+import com.xworkz.inheretence.internal.Ticket;
+
+public class TicketRunner {
+        public static void main(String[] args) {
+
+            Ticket digitalTicket = new DigitalTicket();
+
+            Ticket physicalTicket = new Ticket();
+        }
+    }
