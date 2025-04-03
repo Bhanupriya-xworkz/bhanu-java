beba332192ae265689dfae2509dabbef82f8b822
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-04-03 22:04:38 +0530

    add Switch file

--- Assess/src/com/xworkz/inheretence/external/LaptopRunner.java
+++ Assess/src/com/xworkz/inheretence/external/LaptopRunner.java
@@ -1,11 +1,29 @@
 package com.xworkz.inheretence.external;
-import com.xworkz.inheretence.internal.LaptopDevice;
+
+import com.xworkz.inheretence.internal.Laptop;
 import com.xworkz.inheretence.internal.MacBook;
 
 public class LaptopRunner {
+    public class LaptopRunner {
         public static void main(String[] args) {
-            LaptopDevice laptop = new MacBook();
 
-            LaptopDevice laptopDevice = new LaptopDevice();
+            Laptop laptop = new MacBook();
+
+            Laptop basicLaptop = new Laptop();
+
+            laptop.startLaptop();
+            laptop.checkBattery();
+            laptop.updateSoftware();
+            laptop.connectToWiFi();
+            laptop.turnOff();
+            ((MacBook) laptop).enableTouchBar();
+            ((MacBook) laptop).enableSiri();
+            ((MacBook) laptop).enableMacOSFeatures();
+
+            basicLaptop.startLaptop();
+            basicLaptop.checkBattery();
+            basicLaptop.updateSoftware();
         }
     }
+
+
