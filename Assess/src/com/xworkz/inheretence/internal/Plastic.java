1d0b10a80808f1dcc7a3182f263481922fae1a7b
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-04-04 00:06:45 +0530

    add Plastic file

---
+++ Assess/src/com/xworkz/inheretence/internal/Plastic.java
@@ -0,0 +1,28 @@
+package com.xworkz.inheretence.internal;
+
+public class Plastic {
+        public void P()
+        {
+            System.out.println("no-args const of plastic");
+        }
+
+        public void manufacture()
+        {
+            System.out.println("plastic is being manufactured");
+        }
+
+        public void use()
+        {
+            System.out.println("plastic is being used");
+        }
+
+        public void dispose()
+        {
+            System.out.println("plastic is being disposed");
+        }
+
+        public void pollute()
+        {
+            System.out.println("plastic is polluting the environment");
+        }
+    }
