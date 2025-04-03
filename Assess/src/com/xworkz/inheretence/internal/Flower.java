1a60d1dbce89ebe577fd3d3048262f94f5bb09ca
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-04-04 01:40:33 +0530

    add Flower file

---
+++ Assess/src/com/xworkz/inheretence/internal/Flower.java
@@ -0,0 +1,24 @@
+package com.xworkz.inheretence.internal;
+
+    public class Flower {
+        public void F() {
+            System.out.println("no-args const of flower");
+        }
+
+        public void bloom() {
+            System.out.println("flower is blooming");
+        }
+
+        public void wither() {
+            System.out.println("flower is withering");
+        }
+
+        public void fragrance() {
+            System.out.println("flower has a pleasant fragrance");
+        }
+
+        public void color() {
+            System.out.println("flower has vibrant colors");
+        }
+    }
+
