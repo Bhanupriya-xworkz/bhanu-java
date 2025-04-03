360676c56ce18911bdba50aedf55790be293813d
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-04-04 00:19:22 +0530

    add Cap file

---
+++ Assess/src/com/xworkz/inheretence/external/CapRunner.java
@@ -0,0 +1,12 @@
+package com.xworkz.inheretence.external;
+
+import com.xworkz.inheretence.internal.BaseballCap;
+import com.xworkz.inheretence.internal.Cap;
+
+public class CapRunner {
+        public static void main(String[] args) {
+            Cap cap = new BaseballCap();
+            Cap basicCap = new Cap();
+        }
+    }
+
---
+++ Assess/src/com/xworkz/inheretence/internal/Cap.java
@@ -0,0 +1,29 @@
+package com.xworkz.inheretence.internal;
+
+public class Cap {
+        public void C()
+        {
+            System.out.println("no-args const of cap");
+        }
+
+        public void wear()
+        {
+            System.out.println("cap is being worn");
+        }
+
+        public void remove()
+        {
+            System.out.println("cap is being removed");
+        }
+
+        public void adjustFit()
+        {
+            System.out.println("adjusting the fit of the cap");
+        }
+
+        public void store()
+        {
+            System.out.println("cap is being stored");
+        }
+    }
+
