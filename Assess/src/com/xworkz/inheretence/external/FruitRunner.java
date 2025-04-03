1e2653c75fd7c6a583abac5a44248eb49b963d45
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-04-03 23:58:02 +0530

    add Fruit file

---
+++ Assess/src/com/xworkz/inheretence/external/FruitRunner.java
@@ -0,0 +1,13 @@
+package com.xworkz.inheretence.external;
+
+import com.xworkz.inheretence.internal.CitrusFruit;
+import com.xworkz.inheretence.internal.Fruit;
+
+
+public class FruitRunner {
+        public static void main(String[] args) {
+            Fruit fruit = new CitrusFruit();
+            Fruit basicFruit = new Fruit();
+        }
+    }
+
---
+++ Assess/src/com/xworkz/inheretence/internal/Fruit.java
@@ -0,0 +1,29 @@
+package com.xworkz.inheretence.internal;
+
+    public class Fruit {
+        public void F()
+        {
+            System.out.println("no-args const of fruit");
+        }
+
+        public void grow()
+        {
+            System.out.println("fruit is growing");
+        }
+
+        public void ripen()
+        {
+            System.out.println("fruit is ripening");
+        }
+
+        public void harvest()
+        {
+            System.out.println("fruit is harvested");
+        }
+
+        public void eat()
+        {
+            System.out.println("fruit is being eaten");
+        }
+    }
+
