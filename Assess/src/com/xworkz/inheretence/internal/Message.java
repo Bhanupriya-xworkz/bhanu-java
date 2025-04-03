db86cf2ed724a7a8afecea35dabbefe946757ac1
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-04-03 20:37:11 +0530

    add Message file

---
+++ Assess/src/com/xworkz/inheretence/internal/Message.java
@@ -0,0 +1,29 @@
+package com.xworkz.inheretence.internal;
+
+public class Message {
+
+        // Constructor for Message
+        public Message() {
+            System.out.println("No-args constructor of Message");
+        }
+
+        public void composeMessage() {
+            System.out.println("Composing a traditional message...");
+        }
+
+        public void sendMessage() {
+            System.out.println("Sending a traditional message...");
+        }
+
+        public void receiveMessage() {
+            System.out.println("Receiving a traditional message...");
+        }
+
+        public void deleteMessage() {
+            System.out.println("Deleting the traditional message...");
+        }
+
+        public void displayMessage() {
+            System.out.println("Displaying the traditional message...");
+        }
+    }
