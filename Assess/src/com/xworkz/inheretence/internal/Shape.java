86fd877a55dd40cabaf46491f74906ca28ed0ee1
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-04-03 23:25:47 +0530

    add Shape file

---
+++ Assess/src/com/xworkz/inheretence/internal/Shape.java
@@ -0,0 +1,24 @@
+package com.xworkz.inheretence.internal;
+public class Shape {
+
+        public Shape() {
+            System.out.println("No-args constructor of Shape");
+        }
+
+        public void draw() {
+            System.out.println("Drawing the shape...");
+        }
+
+        public void resize() {
+            System.out.println("Resizing the shape...");
+        }
+
+        public void calculateArea() {
+            System.out.println("Calculating the area of the shape...");
+        }
+
+        public void calculatePerimeter() {
+            System.out.println("Calculating the perimeter of the shape...");
+        }
+    }
+
