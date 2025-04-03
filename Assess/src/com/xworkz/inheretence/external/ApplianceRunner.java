f47a3620848061f4d502cf2bcfca5cb1bb6e450b
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-04-03 23:19:14 +0530

    add Appliance file

---
+++ Assess/src/com/xworkz/inheretence/external/ApplianceRunner.java
@@ -0,0 +1,14 @@
+package com.xworkz.inheretence.external;
+
+import com.xworkz.inheretence.internal.Appliance;
+import com.xworkz.inheretence.internal.WashingMachine;
+
+public class ApplianceRunner {
+        public static void main(String[] args) {
+
+            Appliance appliance = new WashingMachine();
+
+            Appliance basicAppliance = new Appliance();
+        }
+    }
+
