30f73a7ff831c2c0ea872c7eb9de1082feb8efd9
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-04-03 23:12:23 +0530

    add Rubber file

---
+++ Assess/src/com/xworkz/inheretence/external/RubberRunner.java
@@ -0,0 +1,13 @@
+package com.xworkz.inheretence.external;
+
+import com.xworkz.inheretence.internal.Balloon;
+import com.xworkz.inheretence.internal.Rubber;
+
+public class RubberRunner {
+        public satic void main(String[] args) {
+
+            Rubber rubber = new Balloon();
+
+            Rubber basicRubber = new Rubber();
+        }
+    }
---
+++ Assess/src/com/xworkz/inheretence/internal/Rubber.java
@@ -0,0 +1,28 @@
+package com.xworkz.inheretence.internal;
+
+public class Rubber {
+
+        public Rubber() {
+            System.out.println("No-args constructor of Rubber");
+        }
+
+        public void stretch() {
+            System.out.println("Stretching the rubber...");
+        }
+
+        public void bounce() {
+            System.out.println("Bouncing the rubber...");
+        }
+
+        public void mold() {
+            System.out.println("Molding the rubber...");
+        }
+
+        public void roll() {
+            System.out.println("Rolling the rubber...");
+        }
+
+        public void tear() {
+            System.out.println("Tearing the rubber...");
+        }
+    }
