688ebdd8186da4b8aff99e655a3b133f0c5a7dd5
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-04-03 18:31:11 +0530

    add ShoppingCart file

---
+++ Assess/src/com/xworkz/inheretence/external/ShoppingCartRunner.java
@@ -0,0 +1,13 @@
+package com.xworkz.inheretence.external;
+
+import com.xworkz.inheretence.internal.DigitalShoppingCart;
+import com.xworkz.inheretence.internal.ShoppingCart;
+
+public class ShoppingCartRunner {
+        public static void main(String[] args) {
+
+            ShoppingCart digitalCart = new DigitalShoppingCart();
+
+            ShoppingCart basicCart = new ShoppingCart();
+        }
+    }
