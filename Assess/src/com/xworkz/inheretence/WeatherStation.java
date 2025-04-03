1ae950f7c3be99999a63c06669131c213d448ee8
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-04-03 18:52:25 +0530

    add WeatherStation file

---
+++ Assess/src/com/xworkz/inheretence/WeatherStation.java
@@ -0,0 +1,28 @@
+package com.xworkz.inheretence;
+
+public class WeatherStation {
+
+        public WeatherStation() {
+            System.out.println("No-args constructor of WeatherStation");
+        }
+
+        public void checkTemperature() {
+            System.out.println("Checking the temperature...");
+        }
+
+        public void checkHumidity() {
+            System.out.println("Checking the humidity...");
+        }
+
+        public void checkPressure() {
+            System.out.println("Checking the air pressure...");
+        }
+
+        public void displayWeather() {
+            System.out.println("Displaying the current weather...");
+        }
+
+        public void calibrateStation() {
+            System.out.println("Calibrating the weather station...");
+        }
+    }
---
+++ Assess/src/com/xworkz/inheretence/external/WheatherStationRunner.java
@@ -0,0 +1,13 @@
+package com.xworkz.inheretence.external;
+
+import com.xworkz.inheretence.WeatherStation;
+import com.xworkz.inheretence.internal.DigitalWeatherStation;
+
+public class WheatherStationRunner {
+        public static void main(String[] args) {
+
+            WeatherStation digitalWeatherStation = new DigitalWeatherStation();
+
+            WeatherStation basicWeatherStation = new WeatherStation();
+        }
+    }
