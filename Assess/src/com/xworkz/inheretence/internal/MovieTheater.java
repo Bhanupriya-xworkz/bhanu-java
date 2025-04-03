28b15f9bdc3b9ced2bd0c27c00e23ceafe46d0c0
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-04-03 18:59:09 +0530

    add MovieTheator file

---
+++ Assess/src/com/xworkz/inheretence/internal/MovieTheater.java
@@ -0,0 +1,28 @@
+package com.xworkz.inheretence.internal;
+
+public class MovieTheater {
+
+        public MovieTheater() {
+            System.out.println("No-args constructor of MovieTheater");
+        }
+
+        public void checkSeats() {
+            System.out.println("Checking the availability of seats...");
+        }
+
+        public void playMovie() {
+            System.out.println("Playing the movie in the theater...");
+        }
+
+        public void checkSoundQuality() {
+            System.out.println("Checking the sound quality in the theater...");
+        }
+
+        public void checkScreenResolution() {
+            System.out.println("Checking the screen resolution in the theater...");
+        }
+
+        public void manageTicketBooking() {
+            System.out.println("Managing ticket bookings...");
+        }
+    }
