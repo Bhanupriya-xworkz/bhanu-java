783cadeb87db1e671b1dbaf1f2135ca19802acc0
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-04-04 01:00:46 +0530

    add Work file

---
+++ Assess/src/com/xworkz/inheretence/internal/Work.java
@@ -0,0 +1,29 @@
+package com.xworkz.inheretence.internal;
+
+    public class Work {
+        public void W()
+        {
+            System.out.println("no-args const of work");
+        }
+
+        public void start()
+        {
+            System.out.println("work is starting");
+        }
+
+        public void pause()
+        {
+            System.out.println("work is paused");
+        }
+
+        public void finish()
+        {
+            System.out.println("work is finished");
+        }
+
+        public void review()
+        {
+            System.out.println("work is being reviewed");
+        }
+    }
+
