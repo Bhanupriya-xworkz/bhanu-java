b5d318982db4669f1bc591544b0de32a99ac9db8
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-04-03 23:45:24 +0530

    add Magnet file

---
+++ Assess/src/com/xworkz/inheretence/internal/Magnet.java
@@ -0,0 +1,21 @@
+package com.xworkz.inheretence.internal;
+
+public class Magnet {
+
+        public Magnet() {
+            System.out.println("No-args constructor of Magnet");
+        }
+
+        public void attractMetal() {
+            System.out.println("Attracting metal objects...");
+        }
+
+        public void repel() {
+            System.out.println("Repelling the opposite magnetic poles...");
+        }
+
+        public void displayInfo() {
+            System.out.println("This is a simple magnet.");
+        }
+    }
+
