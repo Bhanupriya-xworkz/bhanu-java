8ed384d5bc9f7d6a797d48b80060251762e25953
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-04-04 08:43:44 +0530

    add Chair file

---
+++ Assess/src/com/xworkz/inheretence/external/ChairRunner.java
@@ -0,0 +1,13 @@
+package com.xworkz.inheretence.external;
+
+
+import com.xworkz.inheretence.external.OfficeChair;
+import com.xworkz.inheretence.external.Chair;
+
+    public class ChairRunner {
+        public static void main(String[] args) {
+            Chair chair = new OfficeChair();
+            Chair officeChair = new Chair();
+        }
+    }
+
