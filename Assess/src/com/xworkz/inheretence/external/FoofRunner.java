71d92af77ac3cbe8183e710c4af7ad1fbb549b28
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-04-05 23:28:05 +0530

    add Friut file

---
+++ Assess/src/com/xworkz/inheretence/external/FoofRunner.java
@@ -0,0 +1,14 @@
+package com.xworkz.inheretence.external;
+
+import com.xworkz.inheretence.internal.Food;
+import com.xworkz.inheretence.internal.Fruit;
+
+public class FoodRunner {
+        public static void main(String[] args) {
+            Food food = new Friut();
+            food.foodTypes();
+
+            Food basicFood = new Food();
+        }
+    }
+
