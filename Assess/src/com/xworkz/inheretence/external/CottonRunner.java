4aff3cec8e24808b0ea073531fb667e98bfa168d
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-04-05 20:54:35 +0530

    add Cotton file

---
+++ Assess/src/com/xworkz/inheretence/external/CottonRunner.java
@@ -0,0 +1,15 @@
+package com.xworkz.inheretence.external;
+
+import com.xworkz.inheretence.internal.Notebook;
+import com.xworkz.inheretence.internal.Cotton;
+
+public class CottonRunner {
+        public static void main(String[] args) {
+
+            Cotton cotton = new Notebook();
+
+            Cotton simpleCotton = new Cotton();
+            simpleCotton.write();
+        }
+    }
+
---
+++ Assess/src/com/xworkz/inheretence/internal/Cotton.java
@@ -0,0 +1,30 @@
+package com.xworkz.inheretence.internal;
+
+public class Cotton {
+    public class Cotton{
+
+            System.out.println("no-args constructor of Cotton");
+        }
+
+        // Methods of Paper
+        public void write() {
+            System.out.println("Writing on the Cotton");
+        }
+
+        public void tear() {
+            System.out.println("Tearing the Cotton");
+        }
+
+        public void fold() {
+            System.out.println("Folding the Cotton");
+        }
+
+        public void crumple() {
+            System.out.println("Crumpling the Cotton");
+        }
+
+        public void burn() {
+            System.out.println("Burning the Cotton");
+        }
+    }
+
