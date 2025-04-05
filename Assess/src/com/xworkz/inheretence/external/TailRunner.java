b8812eeb9ce6f4b727ebd95712f4eb6d95543878
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-04-05 23:41:39 +0530

    add Tail file

---
+++ Assess/src/com/xworkz/inheretence/external/TailRunner.java
@@ -0,0 +1,14 @@
+package com.xworkz.inheretence.external;
+
+import com.xworkz.inheretence.internal.AnimalTail;
+import com.xworkz.inheretence.internal.Tail;
+
+public class TailRunner {
+        public static void main(String[] args) {
+            Tail tail = new AnimalTail();
+            tail.tailTypes();
+
+            Tail basicTail = new Tail();
+        }
+    }
+
---
+++ Assess/src/com/xworkz/inheretence/internal/Tail.java
@@ -0,0 +1,24 @@
+package com.xworkz.inheretence.internal;
+
+    public class Tail {
+        public void tailInfo() {
+            System.out.println("A tail is an elongated part that extends from the back of an animal's body.");
+        }
+
+        public void tailTypes() {
+            System.out.println("Tails come in various types, including fluffy tails, thin tails, and short tails.");
+        }
+
+        public void tailFunction() {
+            System.out.println("Tails serve multiple purposes such as balancing, communication, and protection.");
+        }
+
+        public void tailMovement() {
+            System.out.println("Tails can move in different ways: wagging, swishing, and flicking, depending on the species.");
+        }
+
+        public void tailCare() {
+            System.out.println("Tails need care like any other body part, particularly in animals with long, thick tails.");
+        }
+    }
+
