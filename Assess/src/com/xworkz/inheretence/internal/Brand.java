39e2544869f134963db23cb0687fd925b1da7376
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-04-05 21:29:19 +0530

    add Brand file

---
+++ Assess/src/com/xworkz/inheretence/internal/Brand.java
@@ -0,0 +1,24 @@
+package com.xworkz.inheretence.internal;
+
+    public class Brand {
+        public void brandInfo() {
+            System.out.println("This is a basic brand.");
+        }
+
+        public void marketPosition() {
+            System.out.println("The brand is positioned in the general market.");
+        }
+
+        public void targetAudience() {
+            System.out.println("The brand targets a wide audience.");
+        }
+
+        public void productLine() {
+            System.out.println("The brand has a wide range of products.");
+        }
+
+        public void advertising() {
+            System.out.println("The brand uses general advertising methods.");
+        }
+    }
+
