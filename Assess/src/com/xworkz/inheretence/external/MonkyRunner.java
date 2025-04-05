6f4b7d15924c2f9cab60b81838d4ff5519e553f5
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-04-05 22:50:58 +0530

    add Sky file

---
+++ Assess/src/com/xworkz/inheretence/external/MonkyRunner.java
@@ -0,0 +1,14 @@
+package com.xworkz.inheretence.external;
+
+import com.xworkz.inheretence.internal.Chimpanzee;
+import com.xworkz.inheretence.internal.Monkey;
+
+public class MonkeyRunner {
+        public static void main(String[] args) {
+            Monkey monkey = new Chimpanzee();
+            monkey.habitat();
+
+            Monkey basicMonkey = new Monkey();
+        }
+    }
+
