bc7b0115bca4694fbbc384ead2d7cf980cffa04c
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-04-05 21:50:13 +0530

    add Programmingcode file

---
+++ Assess/src/com/xworkz/inheretence/internal/AdvancedProgrammingCode.java
@@ -0,0 +1,28 @@
+package com.xworkz.inheretence.internal;
+
+    public class AdvancedProgrammingcode extends Programmingcode {
+        public void advancedInfo() {
+            System.out.println("This is advanced programming code with optimized techniques.");
+        }
+
+        {
+            super.syntax();
+            System.out.println("Advanced programming code may follow more complex syntax rules.");
+        }
+
+        {
+            super.logic();
+            System.out.println("Advanced programming code involves sophisticated algorithms and data structures.");
+        }
+
+        {
+            super.structure();
+            System.out.println("Advanced programming code follows principles of software design patterns and architecture.");
+        }
+
+        {
+            super.debugging();
+            System.out.println("Advanced programming code uses advanced debugging tools and techniques.");
+        }
+    }
+
---
+++ Assess/src/com/xworkz/inheretence/internal/Programmingcode.java
@@ -0,0 +1,24 @@
+package com.xworkz.inheretence.internal;
+
+public class Programmingcode {
+        public void codeInfo() {
+            System.out.println("This is basic programming code.");
+        }
+
+        public void syntax() {
+            System.out.println("Programming code follows specific syntax rules.");
+        }
+
+        public void logic() {
+            System.out.println("Programming code has logic to solve problems.");
+        }
+
+        public void structure() {
+            System.out.println("Programming code is organized into functions, classes, or modules.");
+        }
+
+        public void debugging() {
+            System.out.println("Programming code can be debugged to fix errors.");
+        }
+    }
+
