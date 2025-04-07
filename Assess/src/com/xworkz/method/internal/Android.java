2f56e8963276dbdfa859ae5da32eab58415c9576
bhanu <bhanupriyabhanupriya176@gmail.com>
2025-04-07 08:42:29 +0530

    add Robot file

---
+++ Assess/src/com/xworkz/method/internal/Android.java
@@ -0,0 +1,27 @@
+package com.xworkz.method.internal;
+
+public class Android extends Robot{
+    public Android() {
+        System.out.println("Android is created...");
+    }
+    @Override
+     public void crawl(){
+        System.out.println("Android walks like a human...");
+     }
+   @Override
+    public void fly(){
+        System.out.println("Android can talk naturally...");
+     }
+   @Override
+    public void hide(){
+        System.out.println("Android uses AI to rocognize voices...");
+     }
+     @Override
+    public void eat(){
+        System.out.println("Android is performing complex tasks...");
+     }
+   @Override
+    public void shedSkin (){
+        System.out.println("Android is recharging wirelessly...");
+     }
+}
---
+++ Assess/src/com/xworkz/method/internal/Robot.java
@@ -0,0 +1,38 @@
+package com.xworkz.method.internal;
+
+public class Robot {
+
+    public Robot() {
+        System.out.println("No-args constructor of cockroach");
+    }
+
+    public void crawl() {
+        System.out.println("Cockroach is crawling...");
+    }
+
+    public void fly() {
+
+        System.out.println("Cockroach is flying...");
+    }
+
+    public void hide() {
+
+        System.out.println("Cockroach is hiding...");
+    }
+
+    public void eat() {
+
+        System.out.println("Cockroach is eating...");
+    }
+
+    public void shedSkin() {
+
+        System.out.println("Cockroach is shedding its skin...");
+    }
+
+    public void reproduce() {
+
+        System.out.println("Cockroach is reproducing...");
+    }
+}
+
